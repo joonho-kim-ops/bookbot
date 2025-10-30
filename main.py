a2317ef328f79b0eed879d0ca0578e6705fc0028
joonho-kim-ops <joonhok79@gmail.com>
2025-10-30 09:20:40 -0400

    add letter counter

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from stats import word_count
+from stats import char_count
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     print(f"Found {word_count(book_text)} total words")
 
+
+
+    print(f"Processed characters: {char_count(book_text)}")
+
 def get_book_text(file):
     with open (file) as f:
         file_contents = f.read()
     return file_contents
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,33 @@
+from collections import Counter
+
 def word_count(text):
     words = text.split()
     num_words = len(words)
     return num_words
+
+# Function to count letters in the text
+# Not needed for main.py, but included for completeness
+"""
+def letter_count(text):
+    letters = [char for char in text if char.isalpha()]
+    num_letters = len(letters)
+    return num_letters
+"""
+
+def char_count(text):
+    lower_text = text.lower()
+    char_count = Counter(lower_text)
+    return char_count
+
+"""
+# Manuel counter alternative
+def char_count(text):
+    chars = {}
+    for char in text.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+"""
+
