d159be69816b53fb1b2fa42207be0299022a9fac
joonho-kim-ops <joonhok79@gmail.com>
2025-10-29 23:40:35 -0400

    word_count created

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    print(book_text)
+    print(f"Found {word_count(book_text)} total words")
 
 def get_book_text(file):
     with open (file) as f:
         file_contents = f.read()
     return file_contents
+
+def word_count(text):
+    words = text.split()
+    num_words = len(words)
+    return num_words
 
 main()
